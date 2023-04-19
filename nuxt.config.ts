29c7df01540b0693ec7784a05a491cdebcd2e8b3
blackburn32 <alexblackburn32@gmail.com>
2023-04-19 14:01:05 -0700

    Fix favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 export default defineNuxtConfig({
     app: {
         baseURL: '/metronome/',
+        cdnURL: '/metronome/',
     },
     ssr: false,
     modules: ['@nuxtjs/tailwindcss', 'nuxt-icon'],
